59e9381dadae62cf86c3bfb99c218698bb005069
Italo Vieira <italolvv@hotmail.com>
2023-02-07 20:39:10 -0300

    add struct back end

---
+++ prisma/migrations/20230118171059_add_table_habit_week_days/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "habit_week_days" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "habit_id" TEXT NOT NULL,
+    "week_day" INTEGER NOT NULL,
+    "created_at" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "habit_week_days_habit_id_week_day_key" ON "habit_week_days"("habit_id", "week_day");
